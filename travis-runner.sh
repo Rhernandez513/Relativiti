b5fda62db3c9c0611371dd6676361ede455cf963
Robert Hernandez <rhernandez513@gmail.com>
2016-03-03 19:44:06 -0600

    attemping to fix travis build

--- travis-runner.sh
+++ travis-runner.sh
@@ -3,7 +3,7 @@ set -o pipefail
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
-  git config --global user.email "samccone@gmail.com" && \
+  git config --global user.email "rhernandez513@gmail.com" && \
   git config --global user.name "auto deployer" && \
   echo "Deploying!" && \
   sed -i.tmp "s/\/\/ app.baseUrl = '\/polymer-starter-kit/app.baseUrl = '\/polymer-starter-kit/" app/scripts/app.js && \
