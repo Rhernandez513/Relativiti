88e1e05d51dc78dd93f8ac9318625c701a3c40b3
auto deployer <rhernandez513@gmail.com>
2016-03-04 14:00:46 -0600

    potential travis fix

--- travis-runner.sh
+++ travis-runner.sh
@@ -6,11 +6,11 @@ then
   git config --global user.email "rhernandez513@gmail.com" && \
   git config --global user.name "auto deployer" && \
   echo "Deploying!" && \
-  sed -i.tmp "s/\/\/ app.baseUrl = '\/sheet-music-app/app.baseUrl = '\/sheet-music-app/" app/scripts/app.js && \
+  sed -i.tmp "s/\/\/ app.baseUrl = '\/your-pathname/app.baseUrl = '\/your-pathname/" app/scripts/app.js && \
   rm app/scripts/app.js.tmp && \
   bower i && \
-  gulp build-deploy-gh-pages && \
-  sed -i.tmp "s/app.baseUrl = '\/sheet-music-app/\/\/ app.baseUrl = '\/sheet-music-app/" app/scripts/app.js && \
+  gulp deploy-gh-pages && \
+  sed -i.tmp "s/app.baseUrl = '\/your-pathname/\/\/ app.baseUrl = '\/your-pathname /" app/scripts/app.js && \
   rm app/scripts/app.js.tmp
 else
   npm run lint
