d534c1ab7da561e77688e857f5a5127af308dccd
Robert David Hernandez <rhernandez513@gmail.com>
2016-03-06 17:11:20 -0600

    Using TravisCI Env Variables + git command

--- travis-runner.sh
+++ travis-runner.sh
@@ -9,10 +9,16 @@ set -o pipefail
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
-  git config --global user.email "Rhernandez513@gmail.com" && \
-  git config --global user.name "auto deployer" && \
-  echo "github password is: $pass" && \
-  git config --global user.password "$pass" && \
+
+# Info that shows up on Github
+  git config --global user.email "$GH_USER" && \
+  git config --global user.name "auto deployer"
+
+# Attempt to use TravisCI Env Variables to set git credentials
+  touch ~/.git-credentials && \
+  echo "https://$GH_USER:$GH_PASS@github.com" > ~/.git-credentials
+
+# Have built back-end worker
   echo "Deploying!" && \
   sed -i.tmp "s/\/\/ app.baseUrl = '\/your-pathname/app.baseUrl = '\/your-pathname/" app/scripts/app.js && \
   rm app/scripts/app.js.tmp && \
