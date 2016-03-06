301a4fec1c8932873cfe33f2c38863bdc925cf23
Robert David Hernandez <rhernandez513@gmail.com>
2016-03-06 15:46:24 -0600

    remove duplicate build step, echo env var to check

--- travis-runner.sh
+++ travis-runner.sh
@@ -9,9 +9,10 @@ set -o pipefail
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
-  git config --global user.email "rhernandez513@gmail.com" && \
+  git config --global user.email "Rhernandez513@gmail.com" && \
   git config --global user.name "auto deployer" && \
-  git config --global user.password "$pass"
+  echo "github password is: $pass" && \
+  git config --global user.password "$pass" && \
   echo "Deploying!" && \
   sed -i.tmp "s/\/\/ app.baseUrl = '\/your-pathname/app.baseUrl = '\/your-pathname/" app/scripts/app.js && \
   rm app/scripts/app.js.tmp && \
