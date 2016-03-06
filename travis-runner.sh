bb319d6a03e7edbb5c3fef6472e49ccbd0b94f21
Robert David Hernandez <rhernandez513@gmail.com>
2016-03-06 14:41:21 -0600

    add password env variable

--- travis-runner.sh
+++ travis-runner.sh
@@ -11,7 +11,7 @@ if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
   git config --global user.email "rhernandez513@gmail.com" && \
   git config --global user.name "auto deployer" && \
-  git config --global user.password $secure
+  git config --global user.password "$pass"
   echo "Deploying!" && \
   sed -i.tmp "s/\/\/ app.baseUrl = '\/your-pathname/app.baseUrl = '\/your-pathname/" app/scripts/app.js && \
   rm app/scripts/app.js.tmp && \
