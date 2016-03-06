eb8bc20082555995e861f4eeea1251c5e8fac800
Robert David Hernandez <rhernandez513@gmail.com>
2016-03-05 23:37:50 -0600

    .

--- travis-runner.sh
+++ travis-runner.sh
@@ -2,13 +2,16 @@
 #
 # Used to build TravisCI Versions of Apps && GitHub Pages version
 #
+# Should run only on dev environments  && TravisCI
+#
 
 set -o pipefail
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
   git config --global user.email "rhernandez513@gmail.com" && \
-  git config --global user.name "Robert David Hernandez" && \
+  git config --global user.name "auto deployer" && \
+  git config --global user.password $secure
   echo "Deploying!" && \
   sed -i.tmp "s/\/\/ app.baseUrl = '\/your-pathname/app.baseUrl = '\/your-pathname/" app/scripts/app.js && \
   rm app/scripts/app.js.tmp && \
