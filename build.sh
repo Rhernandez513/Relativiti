c2588a3f63a0759a8ad5ed0a174616ff9a8f2470
Robert David Hernandez <rhernandez513@gmail.com>
2016-03-09 11:41:29 -0600

    removing sudo in build script, may need to use more pipes

--- build.sh
+++ build.sh
@@ -14,9 +14,9 @@ npm i bower
 bower install
 
 # "Client side" gulp used to build Android APKs via cordova
-sudo gulp --cordova 'platform add android'
-sudo gulp --cordova 'plugin add cordova-plugin-media'
-sudo chown -R $USER .
+gulp --cordova 'platform add android'
+gulp --cordova 'plugin add cordova-plugin-media'
+chown -R $USER .
 gulp --cordova 'build'
 
 # This is in travis runner so I suspect this one is a duplicate
