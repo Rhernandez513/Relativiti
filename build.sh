bb8e42906ecc685bace2ced714b930cd292c9dde
Robert David Hernandez <rhernandez513@gmail.com>
2016-03-09 01:46:58 -0600

    1 button press build & deploy works on my local

--- build.sh
+++ build.sh
@@ -2,8 +2,12 @@
 
 # Install "Client side" gulp shell
 npm i gulp
+# Install "Client side bower"
+npm i bower
 
 # "Client side" gulp used to build Android APKs via cordova
 gulp --cordova 'build'
+
+gulp deploy-gh-pages
 
 #EOF
