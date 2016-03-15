a2b42fc620911d66d05f76b3328592c70a7e4773
rhernandez513 <rhernandez513@gmail.com>
2016-03-15 01:02:13 -0500

    change child process to run in #!/usr/bin/env bash

--- travis-pre-build.sh
+++ travis-pre-build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
 # http://RobertHernandez.io/
 # 
@@ -6,7 +6,7 @@
 
 touch ~/.bashrc
 rm -rf "$NVM_DIR"
-touch update_nvm.sh
+touch install_nvm.sh 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh > install_nvm.sh
 sh "./install_nvm.sh"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
